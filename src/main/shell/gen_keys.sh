535e94cf65eb325b78bb9dda90fc236e4ec53668
twilliams <twilliams@apache.org>
2014-10-11 20:17:32 -0400

    switch to rsa

--- src/main/shell/gen_keys.sh
+++ src/main/shell/gen_keys.sh
@@ -5,7 +5,7 @@ cd ../../../target
 mkdir -p ./.ssh
 cd .ssh
 echo -n "Creating keys..."
-ssh-keygen -t dsa -P '' -f id_dsa
+ssh-keygen -t rsa -P '' -f id_dsa
 cat id_dsa.pub > authorized_keys
 echo "done."
 }
