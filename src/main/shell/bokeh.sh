752221ce45f103a6534ee7ba153e32c51b327707
twilliams <twilliams@apache.org>
2014-10-23 19:46:08 -0400

    refactor script

--- src/main/shell/bokeh.sh
+++ src/main/shell/bokeh.sh
@@ -1,19 +1,69 @@
 #!/bin/bash
 
-#### Arg validation
-ARGS=0
-COMMAND_NOT_FOUND=127
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-function usage {
- echo "Usage: $(basename $0)"
+: <<DOCUMENTATION
+ -----------------------------------------------
+| bokeh 
+ -----------------------------------------------
+A utility for installing Apache Blur to in a 
+cloud cluster environment supporting testing.
+ ----------------------------------------------
+DOCUMENTATION
+
+function usage { 
+ echo "Usage: $0 -h" 
+ exit 1 
 }
+scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+function cleanup {
+ rm -rf $scratch
+}
+trap cleanup EXIT
 
-if [ "$#" != "$ARGS" ] ;then
-  usage
-  exit 1
-fi
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+set -e
+set -u
+set -o pipefail
+
+IFS=$'\n\t'
+
+EXPECTED_ARGS=":h"
+set +u
+
+while getopts "${EXPECTED_ARGS}" opt; do
+  case $opt in 
+     h)
+       #TODO: Need some trimming here
+       sed -n '/DOCUMENTATION$/,/^DOCUMENTATION/p' $0
+       usage
+      ;;
+     \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;; 
+     
+   esac
+done
+set -u  
+
+#___init
 if [ ! -f ~/.bokeh ] ;then
-  echo "Error: No properties found at ~/.bokeh"
+  echo "Error: No properties found at ~/.bokeh" >&2
   exit 1
 fi 
 
@@ -22,37 +72,33 @@ source ~/.bokeh
 clientkey=$DO_CLIENT_KEY
 clientid=$DO_CLIENT_ID
 
-#### Function defs
+#___functions
 function validate_keys {
- echo -n "Validating provider credentials... "
+ echo -n "Validating provider credentials... " >&2
  ( 
   cd ../libcloud
   python ./validate_creds.py -k "$1" -i "$2" > /dev/null 2>&1
  )  
   if [ "$?" != "0" ] ;then 
-    echo "Error: Invalid login credentials"
+    echo "Error: Invalid login credentials" >&2
     exit 1
   fi
- echo " done."
+ echo " done." >&2
 }
 
 function generate_ssh_keys {
- echo -n "Generating cluster ssh keys... "
+ echo -n "Generating cluster ssh keys... " >&2
  if [ -d ../../../target/.ssh ] ;then
    rm -Rf ../../../target/.ssh
  fi
  ./gen_keys.sh > /dev/null 2>&1
- echo " done."
+ echo " done." >&2
 } 
 
 function validate_packer {
-  echo -n "Validating packer.io install... "
-  packer > /dev/null 2>&1
-  if [ "$?" == "${COMMAND_NOT_FOUND}" ] ;then
-    echo "Error: Packer.io is required and unfound."
-    exit 1
-  fi
-  echo " done." 
+  echo -n "Validating packer.io install... " >&2
+  packer -v > /dev/null 
+  echo " done." >&2
 }
 
 function build_blur_image {
@@ -60,14 +106,12 @@ function build_blur_image {
   packer build -var "do_client_id=$1" -var "do_api_key=$2" -var 'blur_version=apache-blur-0.2.4-incubating-SNAPSHOT-hadoop1' ../packer/blur-node.json
 }
 
-#### Main
+#___main
 
 validate_keys $clientid $clientkey
 
 generate_ssh_keys
 
 validate_packer
-
+echo "validating"
 build_blur_image $clientkey $clientid
-
-
